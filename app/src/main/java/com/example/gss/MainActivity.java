c728415c16bbf914752f8479015369c01515d759
grigorevss <avo1185@gmail.com>
2022-05-12 14:55:10 +0700

    added exit and search items to main menu

--- app/src/main/java/com/example/gss/MainActivity.java
+++ app/src/main/java/com/example/gss/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -57,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        //dfghj
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
